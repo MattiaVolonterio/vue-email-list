02d9295ab959ebbcd10bf2a3285e1982396aa9e9
MattiaVolonterio <volonteriomattia@gmail.com>
2024-02-06 15:41:56 +0100

    feat: mail genartion from api and mail count selection

--- js/main.js
+++ js/main.js
@@ -2,12 +2,28 @@ const { createApp } = Vue;
 
 createApp({
   data() {
-    return {};
+    return {
+      generatedMailArray: [],
+      mailCount: 1,
+    };
   },
 
   computed: {},
 
-  methods: {},
+  methods: {
+    fetchMail() {
+      this.generatedMailArray = [];
+      for (let i = 0; i < this.mailCount; i++) {
+        axios
+          .get("https://flynn.boolean.careers/exercises/api/random/mail")
+          .then((resp) => {
+            const randomEmail = resp.data.response;
+
+            this.generatedMailArray.push(randomEmail);
+          });
+      }
+    },
+  },
 
   mounted() {},
 }).mount("#app");
